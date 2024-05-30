09e56124edc81f9301b98ff0d791579d1ebd106b
ellaclz <jacaluza@s.sfusd.edu>
2024-05-30 08:39:49 -0700

    New Background

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,26 @@
 extends Node
 
 var score = 0
+var total_coins = 20
+var collected_coins = 20
+
 
 @onready var score_label = $ScoreLabel
 
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + "coins."
+
+func collect_coin():
+	collected_coins ++ 1
+	if collected_coins >= total_coins:
+		switch_scene()
+
+func switch_scene():
+	var next_scene_path = "res://scenes/level_two.tscn"
+	var next_scene = load("res://scenes/level_two.tscn")
+	
+	if next_scene != null:
+		get_tree().get_root().free()
+		get_tree().change_scene("res://scenes/level_two.tscn")
+		
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,13 +2,16 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
+const ACCELERATION = 800.0
+const FRICTION = 1000.0
 const JUMP_VELOCITY = -300.0
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
-
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -38,5 +41,17 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >- 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
+	
+func handle_jump():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY/ 2:
+			velocity.y = JUMP_VELOCITY / 2
