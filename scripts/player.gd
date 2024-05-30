de9b417dd2312fed69222832fa1e50c5f0e51667
ellaclz <jacaluza@s.sfusd.edu>
2024-05-30 09:11:06 -0700

    Jump sound

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,6 +10,7 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var sfx_jump = $sfx_jump
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 
@@ -21,6 +22,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		sfx_jump.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
